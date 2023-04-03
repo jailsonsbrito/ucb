bf496e9f0f54368885a9d83640ce842e8999c782
jailsonsbritogit config --global user.email jailsonsbrito@gmail.com <jailsonsbrito@gmail.com>
2023-04-02 22:16:27 -0300

    Atualizaçao da estrutura dos códigos

---
+++ programacaoWeb/js/script.js
@@ -0,0 +1,47 @@
+/*
+console.log("Hello World");
+console.log(typeof 13);
+console.log(typeof 1.3);
+console.log(typeof -5);
+console.log(typeof "Jailson");
+console.log(2%3);
+console.log(parseInt("blabla"));
+console.log("Isso é uma string");
+console.log(`Calculadora: \n A soma de 2 + 2 é ${2+2}`);
+console.log(3>10);
+console.log(3 == "3");
+console.log(3 === "3");
+console.log('Jailson' != "Jailson");
+*/
+
+//Strings
+console.log("Jailson");
+console.log('Jailson');
+console.log(`Jailson`);
+
+//Numbers
+
+console.log(50);
+console.log(-200);
+console.log(3.35);
+console.log(6+4);
+
+//Boolean
+
+console.log("Jailson" == "Jailson");
+console.log(4>3);
+console.log(20<20);
+console.log(4!=4);
+console.log(50>=10);
+
+//AND, OR, NOT
+
+console.log(3==3 && 3>3);
+console.log(3==3 || 3>3);
+console.log(!(3==3 && 3>3));
+console.log(!true);
+console.log(!3===3);
+
+//NaN
+
+console.log("Ana" - 3);
